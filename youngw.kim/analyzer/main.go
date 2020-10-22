daa439d261879018214b517323c51a7b21a016e4
YoungWoo <duddn14758@naver.com>
2020-10-23 02:16:51 +0900

    edit

--- youngw.kim/analyzer/main.go
+++ youngw.kim/analyzer/main.go
@@ -9,10 +9,12 @@ import (
 )
 
 type monitorLog struct {
-	time     string
-	tp       float64
-	ddds_scg int
-	ddds_mcg int
+	time              string
+	tp                float64
+	desired_octet_scg int
+	desired_octet_mcg int
+	ddds_scg          int
+	ddds_mcg          int
 }
 
 func standardizeSpaces(s string) string {
@@ -44,23 +46,32 @@ func main() {
 			// parsing time val
 			res_time := strings.FieldsFunc(scanner.Text(), splitTimeCase)
 			log[index].time = res_time[1]
+		} else if strings.Contains(scanner.Text(), "DDDS Count") {
+			res_ddds := strings.Split(standardizeSpaces(scanner.Text()), " ")
+
+			log[index].desired_octet_scg, _ = strconv.Atoi(res_ddds[4])
+			log[index].desired_octet_mcg, _ = strconv.Atoi(res_ddds[9])
+
+		} else if strings.Contains(scanner.Text(), "Desired Octet") {
+			res_ddds := strings.Split(standardizeSpaces(scanner.Text()), " ")
+
+			log[index].ddds_scg, _ = strconv.Atoi(res_ddds[4])
+			log[index].ddds_mcg, _ = strconv.Atoi(res_ddds[9])
+
 		} else if strings.Contains(scanner.Text(), "Dowlink RX") {
 
 			res_tp := strings.Split(standardizeSpaces(scanner.Text()), " ")
 
 			log[index].tp, _ = strconv.ParseFloat(res_tp[10], 64)
 
-			for idx, _ := range res_tp {
-				fmt.Printf("res_tp[%d] : %s\n", idx, res_tp[idx])
-			}
-
-			fmt.Println(res_tp)
 			log[index].tp, _ = strconv.ParseFloat(res_tp[10], 64)
 
 			if strings.Compare(res_tp[11], "Mbps") == 0 {
 				log[index].tp *= 0.001
 			} else if strings.Compare(res_tp[11], "Kbps") == 0 {
 				log[index].tp *= 0.000001
+			} else if strings.Compare(res_tp[11], "bps") == 0 {
+				log[index].tp = 0
 			}
 
 			avg += log[index].tp
@@ -73,7 +84,8 @@ func main() {
 
 	}
 	for i := 0; i < index; i++ {
-		fmt.Printf("%s %0.3fGbps\n", log[i].time, log[i].tp)
+		fmt.Printf("time : %s\n desired_scg : %d desired_mcg : %d\n ddds_scg : %d ddds_mcg : %d\n  tp : %0.2fMbps\n-----------------\n",
+			log[i].time, log[i].desired_octet_scg, log[i].desired_octet_mcg, log[i].ddds_scg, log[i].ddds_mcg, log[i].tp)
 	}
-	fmt.Printf("avg : %0.3fGbps\n", float64(avg)/float64(index))
+	fmt.Printf("avg : %05.2fMbps\n", float64(avg)/float64(index))
 }
